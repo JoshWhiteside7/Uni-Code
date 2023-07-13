d65f46e0e061b63bf34bbecf68ae2ba83d1cc25e
Josh Whiteside <jwhiteside@aiimi.com>
2023-07-13 10:46:31 +0100

    group size

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from flask import Flask, render_template
+import random
 app = Flask(__name__)
+
+def get_group_number(profile):
+    if profile == "Date Night":
+        return 2
+    elif profile == "Corporate Event":
+        return random.randint(5, 10)
+    elif profile == "Over 21 (age)" or profile == "Under 21 (age)" or profile == "Intergenerational":
+        return random.randint(2, 10)
+    else:
+        return 0  # Default case if the profile doesn't match any specific group number logic
 
 @app.route('/')
 def leaderboard():
@@ -8,7 +19,8 @@ def leaderboard():
         next(f)  # Skip the header line
         for line in f:
             group_name, profile, time = line.strip().split('\t')
-            leaderboard.append((group_name, profile, time))
+            group_number = get_group_number(profile)
+            leaderboard.append((group_name, profile, group_number, time))
     return render_template('leaderboard.html', leaderboard=leaderboard)
 
 if __name__ == '__main__':
